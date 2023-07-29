f5dcf605a748feb15ca1268c63f2772c1a89146a
mayiPalacios <00018619@uca.edu.sv>
2023-07-29 11:21:43 -0600

    :sparkles: add react route and sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import "./App.css";
 import MovieForm from "./components/MovieForm";
+import MovieFormPage from "./page/MovieFormPage";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1 className='header'>Películas</h1>
-      <MovieForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/form" element={<MovieFormPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { handleSubmit } from "../api/Api";
-
+import Sidebar from "./sidebar";
 const FormWrapper = styled.div`
   max-width: 400px;
   margin: 0 auto;
@@ -31,67 +31,69 @@ const Button = styled.button`
 `;
 
 const MovieForm = () => {
-    const [formData, setFormData] = useState({
-        name: "",
-        date: "",
-        duration: "",
-        budget: ""
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    date: "",
+    duration: "",
+    budget: "",
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-    const handleFormSubmit = async () => {
-        try {
-            await handleSubmit(formData);
-        } catch (error) {
-            console.error("Error submitting data:", error);
-        }
-    };
+  const handleFormSubmit = async () => {
+    try {
+      await handleSubmit(formData);
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
 
-    return (
-        <FormWrapper>
-            <FormField>
-                <Label>Nombre:</Label>
-                <Input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-            </FormField>
-            <FormField>
-                <Label>Fecha de estreno:</Label>
-                <Input
-                    type="text"
-                    name="date"
-                    value={formData.date}
-                    onChange={handleChange}
-                />
-            </FormField>
-            <FormField>
-                <Label>Duración (minutos):</Label>
-                <Input
-                    type="number"
-                    name="duration"
-                    value={formData.duration}
-                    onChange={handleChange}
-                />
-            </FormField>
-            <FormField>
-                <Label>Presupuesto:</Label>
-                <Input
-                    type="number"
-                    name="budget"
-                    value={formData.budget}
-                    onChange={handleChange}
-                />
-            </FormField>
-            <Button onClick={handleFormSubmit}>Guardar</Button>
-        </FormWrapper>
-    );
+  return (
+    <div>
+      <FormWrapper>
+        <FormField>
+          <Label>Nombre:</Label>
+          <Input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </FormField>
+        <FormField>
+          <Label>Fecha de estreno:</Label>
+          <Input
+            type="text"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+        </FormField>
+        <FormField>
+          <Label>Duración (minutos):</Label>
+          <Input
+            type="number"
+            name="duration"
+            value={formData.duration}
+            onChange={handleChange}
+          />
+        </FormField>
+        <FormField>
+          <Label>Presupuesto:</Label>
+          <Input
+            type="number"
+            name="budget"
+            value={formData.budget}
+            onChange={handleChange}
+          />
+        </FormField>
+        <Button onClick={handleFormSubmit}>Guardar</Button>
+      </FormWrapper>
+    </div>
+  );
 };
 
 export default MovieForm;
